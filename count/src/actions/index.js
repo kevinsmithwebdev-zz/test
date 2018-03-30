345e13c13fc665504025281ee1209b6ec015b165
Kevin Smith <kevinsmithwebdev@gmail.com>
2018-03-30 14:14:54 -0700

    passing

--- count/src/actions/index.js
+++ count/src/actions/index.js
@@ -1,10 +1,16 @@
 export const incrementCounter = () => {
-  console.log('in incrementCounter action')
   return { type: 'INCREMENT_COUNTER' }
 }
 
 export const decrementCounter = () => {
-  console.log('in decrementCounter action')
   return { type: 'DECREMENT_COUNTER' }
+}
+
+export const resetCounter = () => {
+  return { type: 'RESET_COUNTER' }
+}
+
+export const setCounter = (num) => {
+  return { type: 'SET_COUNTER', num }
 }
  
--- todo/.src/js/app.js
+++
@@ -1,4 +0,0 @@
-const out2 = require('./app2')
-
-out2()
-console.log('here x')
