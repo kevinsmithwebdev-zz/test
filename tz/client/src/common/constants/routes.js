c25ced64bc2efad242305f336878f18b4dbf7bb6
Kevin Smith <kevinsmithwebdev>
2018-05-01 15:03:36 -0700

    passing

--- tz/client/src/common/constants/routes.js
+++
@@ -1,4 +0,0 @@
-const SERVER_URL = "http://localhost:8080"
-
-export const API_LOCATION   = SERVER_URL + '/api/location/'
-export const API_TIMEZONE   = SERVER_URL + '/api/timezone/'
--- tz/client/src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-
-import locations from './locations'
-
-const reducers = combineReducers({
-	locations
-})
-
-export default reducers
