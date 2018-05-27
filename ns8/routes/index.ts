df3a75140d6128064b5f45e1a0099ace8439fc24
Kevin Smith <kevinsmithwebdev@gmail.com>
2018-05-26 23:32:01 -0700

    passing

--- ns8/routes/index.ts
+++ ns8/routes/index.ts
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'NS8 Dev Test', author: 'Kevin Smith', webPage: 'kevinsmithwebdev.com' });
 });
 
 export = router;
