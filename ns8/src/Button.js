28316744d18ba7e9123bc4911d612e64ac41b631
Kevin Smith <kevinsmithwebdev>
2018-05-26 20:40:37 -0700

    passing

---
+++ ns8/src/Button.js
@@ -0,0 +1,22 @@
+const Button = styled.button`
+  color: ${props => props.theme.fg};
+  border: 2px solid ${props => props.theme.fg};
+  background: ${props => props.theme.bg};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+`;
+
+// Define our `fg` and `bg` on the theme
+const theme = {
+  fg: 'palevioletred',
+  bg: 'white'
+};
+
+// This theme swaps `fg` and `bg`
+const invertTheme = ({ fg, bg }) => ({
+  fg: bg,
+  bg: fg
+});
